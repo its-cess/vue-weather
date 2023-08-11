13dc00fa94d167edda90e8f5f480144bab02564c
Cecily Toro <torocecily@gmail.com>
2023-08-11 12:15:32 -0400

    make API calls to fetch city data. display info on cityView component.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
         class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
       >
         <p v-if="searchError">Sorry, something went wrong. Please try again.</p>
-        <p v-if="!serverError && mapboxSearchResults.length === 0">
+        <p v-if="!searchError && mapboxSearchResults.length === 0">
           No results match your query, try a different term.
         </p>
         <!-- Cannot use a v-for and v-else directive on the same element, 
@@ -23,6 +23,7 @@
             v-for="searchResult in mapboxSearchResults"
             :key="searchResult.id"
             class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)"
           >
             {{ searchResult.place_name }}
           </li>
@@ -35,6 +36,7 @@
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRouter } from "vue-router";
 
 const mapboxAPIKey =
   "pk.eyJ1IjoiaXRzY2VzcyIsImEiOiJjbGw1dWI2bncwZ3lvM2pwODZoMnd1Z29lIn0.vc5kHVOmqmCVcdMy85CKPw";
@@ -42,6 +44,25 @@ const searchQuery = ref("");
 const queryTimeout = ref(null);
 const mapboxSearchResults = ref(null);
 const searchError = ref(null);
+
+const router = useRouter();
+
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: "cityView",
+    //replaceAll replaces the space before the state with an empty string
+    //params match the params we gave this path in the router file. setting them to the searchResults.
+    params: { state: state.replaceAll(" ", ""), city: city },
+    //sets the query params in the URL to searchResults
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      long: searchResult.geometry.coordinates[0],
+      preview: true
+    }
+  });
+};
 
 const getSeachResults = () => {
   clearTimeout(queryTimeout.value);
